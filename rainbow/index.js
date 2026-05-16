d3b5eba15618fa9e05c01ed20f3e74f7244540ac
isaiahnoelpulidosalazar <isaiahnoelpulidosalazar@gmail.com>
2026-05-16 18:30:17 +0800

    test minified a few js files

--- rainbow/index.js
+++ rainbow/index.js
@@ -1,17 +1,1 @@
-const colors = [
-            "red",
-            "orange",
-            "yellow",
-            "green",
-            "blue",
-            "indigo",
-            "violet"
-        ];
-        let a = 2;
-        setTimeout(() => {
-            document.body.style.backgroundColor = colors[1];
-        }, 1);
-        setInterval(() => {
-            document.body.style.backgroundColor = colors[a];
-            a = (a + 1) % 7;
-        }, 2000);
+const colors=["red","orange","yellow","green","blue","indigo","violet"];let a=2;setTimeout(()=>{document.body.style.backgroundColor=colors[1]},1),setInterval(()=>{document.body.style.backgroundColor=colors[a],a=(a+1)%7},2e3);
