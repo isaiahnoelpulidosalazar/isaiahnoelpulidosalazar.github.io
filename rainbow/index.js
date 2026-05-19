38a0ef835517e894522f026d91ec0bee7be10b37
isaiahnoelpulidosalazar <isaiahnoelpulidosalazar@gmail.com>
2026-05-19 19:53:40 +0800

    move rainbow script

--- rainbow/index.js
+++
@@ -1,1 +0,0 @@
-const colors=["red","orange","yellow","green","blue","indigo","violet"];let a=2;setTimeout(()=>{document.body.style.backgroundColor=colors[1]},1),setInterval(()=>{document.body.style.backgroundColor=colors[a],a=(a+1)%7},2e3);
